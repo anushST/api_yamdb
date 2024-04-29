1da9a11dbb2f7cbe5096ca1eee65f76bca23b261
Levchenko Aleksandr <madlion@mail.ru>
2024-04-29 17:25:15 +0700

    new file:   api_yamdb/api/serializers.py deleted:    api_yamdb/api/tests.py modified:   api_yamdb/api/views.py modified:   api_yamdb/api_yamdb/urls.py deleted:    api_yamdb/reviews/views.py

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework.serializers import ModelSerializer
+
+from reviews.models import Title, Genre, Category, Review, Comment, User
+
+
+class TitleSerializer(ModelSerializer):
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'rating',
+                  'description', 'genre', 'category', 'review')
+
+
+class GenreSerializer(ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug',)
+
+
+class CategorySerializer(ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug',)
+
+
+class ReviewSerializer(ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = ('title', 'title', 'score', 'author', 'pub_date')
+
+
+class CommentSerializer(ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('review', 'author', 'text', 'pub_date')
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'reviews')
