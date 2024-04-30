46c0ffc48c68d3c7dd720b653bbac96b2fb540b7
anushST <anushervon.s.06@gmail.com>
2024-04-30 21:35:05 +0500

    Add extra fields to User model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,18 @@
+"""Models of "users" app."""
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom user model."""
+
+    class UsersType(models.TextChoices):
+        """Users type options."""
+
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(
+        max_length=9, choices=UsersType.choices, default=UsersType.USER)
